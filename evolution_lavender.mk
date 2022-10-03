5ab2f5868a631d423f896a18cb12dd77796f5cc3
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-10-03 13:12:01 +0600

    evo

--- evolution_lavender.mk
+++ evolution_lavender.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := xdroid_lavender
+PRODUCT_NAME := evolution_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
