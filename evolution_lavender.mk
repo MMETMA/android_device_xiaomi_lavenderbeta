dff1a595e3cc4ce02a32e289942eefd31195de78
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-10-03 13:11:41 +0600

    evo

--- evolution_lavender.mk
+++ evolution_lavender.mk
@@ -8,12 +8,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common xDroidOS stuff
-$(call inherit-product, vendor/xdroid/config/common.mk)
+# Inherit some common evolution stuff
+$(call inherit-product, vendor/evolution/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-XDROID_MAINTAINER := Prashant
+TARGET_USES_MINI_GAPPS := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
