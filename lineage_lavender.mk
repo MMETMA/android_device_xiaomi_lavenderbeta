84debef03899b8b2dd3e2f13e404f599b7e2f6e1
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-10-10 02:13:27 +0600

    rice

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -42,3 +42,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="lavender" \
+    PRODUCT_NAME="lavender" \
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGMIXM release-keys"
+
+BUILD_FINGERPRINT :="xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGMIXM:user/release-keys"
