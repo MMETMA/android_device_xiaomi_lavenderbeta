98eeb04e47009f3d7b87a8ac4ce4d5add08beee8
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-10-16 19:37:42 +0600

    Cr13

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -12,24 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-RICE_MAINTAINER := Apex_Not_Legend
-RICE_CHIPSET := Snapdragon660
 TARGET_ENABLE_BLUR := true
-WITH_GMS := false
-
-# Memory properties
-# Tune these properties according to device perfconfigstore.xml(if available)/device capabilities.
-# These props are overridable
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.fw.bg_apps_limit?=48 \
-    persist.sys.fw.use_trim_settings?=true \
-    persist.sys.fw.empty_app_percent?=50 \
-    persist.sys.fw.trim_empty_percent?=100 \
-    persist.sys.fw.trim_cache_percent?=100 \
-    persist.sys.fw.trim_enable_memory?=2147483648 \
-    persist.sys.fw.bservice_age?=120000 \
-    persist.sys.fw.bservice_limit?=6 \
-    persist.sys.fw.bservice_enable?=true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -42,10 +25,3 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="lavender" \
-    PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGMIXM release-keys"
-
-BUILD_FINGERPRINT :="xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGMIXM:user/release-keys"
