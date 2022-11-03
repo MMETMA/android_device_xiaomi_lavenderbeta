447dbc221373303a80ae5193403f5f900841a6fa
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2022-11-03 23:45:29 +0600

    lavender: CherishOS Bringup
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

---
+++ cherish_lavender.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2020-2021 The CherishOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CherishOS Stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+
+
+# Inherit from lavender device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 720
+
+# CherishOS Stuff with GApps
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := cherish_lavender
+PRODUCT_DEVICE := lavender
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 7
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="lavender" \
+    PRODUCT_NAME="lavender" \
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGMIXM release-keys"
+
+BUILD_FINGERPRINT :="xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGMIXM:user/release-keys"
+
+# Maintainer Stuff
+CHERISH_BUILD_TYPE := UNOFFICIAL
--- product_config.mk
+++ product_config.mk
@@ -1,4 +1,4 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/cherish_lavender.mk
 
-COMMON_LUNCH_CHOICES := lineage_lavender-userdebug
+COMMON_LUNCH_CHOICES := cherish_lavender-userdebug
